fb913cd85adb06e59179069b7f894f3ec89384be
EJ <ejkiel19@gmail.com>
2024-08-15 21:55:56 +0800

    added another 3x3

--- tictactoe/Form1.Designer.cs
+++ tictactoe/Form1.Designer.cs
@@ -44,6 +44,15 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,7 +64,7 @@
             this.aboutToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(651, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1026, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -181,17 +190,98 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(482, 60);
+            this.label1.Location = new System.Drawing.Point(469, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 40);
+            this.label1.Size = new System.Drawing.Size(92, 40);
             this.label1.TabIndex = 10;
-            this.label1.Text = "Tic Tac Toe\r\n\r\n";
+            this.label1.Text = "Twin Tac Toe\r\n\r\n";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(874, 285);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(120, 111);
+            this.button10.TabIndex = 19;
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(748, 285);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(120, 111);
+            this.button11.TabIndex = 18;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(622, 285);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(120, 111);
+            this.button12.TabIndex = 17;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(874, 168);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(120, 111);
+            this.button13.TabIndex = 16;
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(748, 168);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(120, 111);
+            this.button14.TabIndex = 15;
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(622, 168);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(120, 111);
+            this.button15.TabIndex = 14;
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(622, 51);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(120, 111);
+            this.button16.TabIndex = 13;
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(748, 51);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(120, 111);
+            this.button17.TabIndex = 12;
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(874, 51);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(120, 111);
+            this.button18.TabIndex = 11;
+            this.button18.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(651, 430);
+            this.ClientSize = new System.Drawing.Size(1026, 430);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button18);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button8);
@@ -235,5 +325,14 @@
         private Button button8;
         private Button button9;
         private Label label1;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
     }
 }
