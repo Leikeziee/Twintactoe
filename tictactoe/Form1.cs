ca1d5abd99e5950c2faab938e0c7f8a5f36cb50d
EJ <ejkiel19@gmail.com>
2024-08-15 20:47:21 +0800

    Add project files.

---
+++ tictactoe/Form1.cs
@@ -0,0 +1,36 @@
+namespace tictactoe
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true; //true = x turn, false = o turn
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Computer Software Design - Tic Tac Toe Project by Lactao & Muldera");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
